34f37e7a1cc00d59dabb79f8e46a6d07ebd17d07
SefivanEA <seftivanea@gmail.com>
2026-05-16 11:02:39 +0700

    feat: halaman home user CineMatch

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,7 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/home', function () {
+    return Inertia::render('User/Home');
+})->middleware(['auth', 'verified'])->name('home');
