cea61e750381e2294fd6ef41a0a170189942a24a
Resz <reszcode@gmail.com>
2026-06-22 01:39:05 +0700

    Add Film
    Menambahkan film dari backend

---
+++ be-cinema/app/Http/Controllers/Api/FilmController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Film;
+use Illuminate\Http\Request;
+
+class FilmController extends Controller
+{
+    public function index(Request $request)
+    {
+        $query = Film::with('genre');
+
+        // Fitur Pencarian Judul
+        if ($request->has('search') && $request->search != '') {
+            $query->where('judul', 'like', '%' . $request->search . '%');
+        }
+
+        // Fitur Filter Genre
+        if ($request->has('genre') && $request->genre !== 'Semua') {
+            $query->whereHas('genre', function ($q) use ($request) {
+                $q->where('nama', $request->genre);
+            });
+        }
+
+        // Mengambil semua data (bisa diubah menggunakan ->paginate(10) nanti jika film sudah banyak)
+        $films = $query->orderBy('rating_avg', 'desc')->get();
+
+        return response()->json($films);
+    }
+}
--- be-cinema/routes/api.php
+++ be-cinema/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\FilmController;
 
 // Route Publik (Tidak butuh token)
 Route::post('/register', [AuthController::class, 'register']);
@@ -10,6 +11,7 @@ Route::post('/login', [AuthController::class, 'login']);
 // Route Terproteksi (Hanya bisa diakses jika menyertakan Token Sanctum)
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/films', [FilmController::class, 'index']);
     
     // Endpoint untuk mengambil data user yang sedang login
     Route::get('/user', function () {
