d7327a2b68d30090918d9919e21f903272bde3fd
Resz <reszcode@gmail.com>
2026-06-22 15:25:34 +0700

    feat: add admin management
    Menambahkan logika sebagai admin

--- be-cinema/routes/api.php
+++ be-cinema/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\FilmController;
 use App\Http\Controllers\Api\ProfileController;
+use App\Http\Controllers\Api\AdminController;
 
 // Route Publik (Tidak butuh token)
 Route::post('/register', [AuthController::class, 'register']);
@@ -17,6 +18,18 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::put('/user/profile', [ProfileController::class, 'updateProfile']);
     Route::put('/user/genres', [ProfileController::class, 'updateGenres']);
     Route::put('/user/password', [ProfileController::class, 'updatePassword']);
+
+    Route::prefix('admin')->group(function () {
+        // Film Method CRUD
+        Route::post('/films', [AdminController::class, 'storeFilm']);
+        Route::post('/films/{id}', [AdminController::class, 'updateFilm']);
+        Route::delete('/films/{id}', [AdminController::class, 'destroyFilm']);
+        Route::patch('/films/{id}/toggle-status', [AdminController::class, 'toggleFilmStatus']);
+        
+        // User Management
+        Route::patch('/users/{id}/toggle-status', [AdminController::class, 'toggleUserStatus']);
+    });
+
     // Endpoint untuk mengambil data user yang sedang login
     Route::get('/user', function () {
         return request()->user()->load('genres');
